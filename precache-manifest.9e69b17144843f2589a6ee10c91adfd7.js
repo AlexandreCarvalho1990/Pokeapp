89160c022d2e52678574734c1bd7ec8852cbddc9
Alexandre Carvalho <6313199-AlexandreCarvalho1990@users.noreply.gitlab.com>
2020-07-02 20:05:03 +0100

    Updates

---
+++ precache-manifest.9e69b17144843f2589a6ee10c91adfd7.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a89f104146fd57e1c7175068a508559d",
+    "url": "/AlexandreCarvalho1990/Pokeapp/index.html"
+  },
+  {
+    "revision": "a489c16d8ec129bb2053",
+    "url": "/AlexandreCarvalho1990/Pokeapp/static/css/2.11a5732d.chunk.css"
+  },
+  {
+    "revision": "d7dffe601579e674dc9d",
+    "url": "/AlexandreCarvalho1990/Pokeapp/static/css/main.62160c98.chunk.css"
+  },
+  {
+    "revision": "a489c16d8ec129bb2053",
+    "url": "/AlexandreCarvalho1990/Pokeapp/static/js/2.cc97394a.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/AlexandreCarvalho1990/Pokeapp/static/js/2.cc97394a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d7dffe601579e674dc9d",
+    "url": "/AlexandreCarvalho1990/Pokeapp/static/js/main.75d0bc1e.chunk.js"
+  },
+  {
+    "revision": "8e1732995cfdbf1bf92a",
+    "url": "/AlexandreCarvalho1990/Pokeapp/static/js/runtime-main.a85f11b0.js"
+  },
+  {
+    "revision": "338a29ce58f7cec143e3af812eca4533",
+    "url": "/AlexandreCarvalho1990/Pokeapp/static/media/NotFound.338a29ce.png"
+  },
+  {
+    "revision": "9cd5403fa2f8a5720c745af4c23db586",
+    "url": "/AlexandreCarvalho1990/Pokeapp/static/media/PokeApp.9cd5403f.apk"
+  },
+  {
+    "revision": "13fee277c81c479c30d400c7c1e05bd8",
+    "url": "/AlexandreCarvalho1990/Pokeapp/static/media/bitcoin_qrcode_big.13fee277.png"
+  },
+  {
+    "revision": "69c53349f1ea08acd8479cead11c325d",
+    "url": "/AlexandreCarvalho1990/Pokeapp/static/media/eevee.69c53349.svg"
+  },
+  {
+    "revision": "79f9ed59da6a5c00126ba51c3053c736",
+    "url": "/AlexandreCarvalho1990/Pokeapp/static/media/fastcatch_perfectthrow.79f9ed59.mp4"
+  },
+  {
+    "revision": "d1a04a815d4033216ce185459f0626a8",
+    "url": "/AlexandreCarvalho1990/Pokeapp/static/media/feat_list.d1a04a81.mp4"
+  },
+  {
+    "revision": "63cf5c9c29a6aaf227ae18370ab1a24e",
+    "url": "/AlexandreCarvalho1990/Pokeapp/static/media/googleplay.63cf5c9c.svg"
+  },
+  {
+    "revision": "d0853bdca8185f478a9e156c337ea4a9",
+    "url": "/AlexandreCarvalho1990/Pokeapp/static/media/logo.d0853bdc.png"
+  },
+  {
+    "revision": "2f732ac2f0f55b9932022c7d0e6dc911",
+    "url": "/AlexandreCarvalho1990/Pokeapp/static/media/noballdrop.2f732ac2.mp4"
+  },
+  {
+    "revision": "9820a5c2523ea7392060ce728d153c0f",
+    "url": "/AlexandreCarvalho1990/Pokeapp/static/media/pikachu-2.9820a5c2.svg"
+  },
+  {
+    "revision": "76bf98dba303d5d570a081b00e640f46",
+    "url": "/AlexandreCarvalho1990/Pokeapp/static/media/pokecoin.76bf98db.svg"
+  },
+  {
+    "revision": "bfa1c9b20846b1cc865768603b55d72d",
+    "url": "/AlexandreCarvalho1990/Pokeapp/static/media/pokemodred.bfa1c9b2.png"
+  },
+  {
+    "revision": "8e7a3d19e25ee87879219d3b1bfecfe4",
+    "url": "/AlexandreCarvalho1990/Pokeapp/static/media/pokestart.8e7a3d19.svg"
+  },
+  {
+    "revision": "7ae094a60c1a118426a54a5f51b16411",
+    "url": "/AlexandreCarvalho1990/Pokeapp/static/media/quick_powerup.7ae094a6.mp4"
+  },
+  {
+    "revision": "5b05a5f33562b4e3d21463a3d24e0e97",
+    "url": "/AlexandreCarvalho1990/Pokeapp/static/media/transfer_everything.5b05a5f3.mp4"
+  }
+]);
